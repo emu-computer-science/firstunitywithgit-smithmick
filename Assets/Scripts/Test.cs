15b3270757dd16c66e45252b6acf092c0feb8a39
Esmit147 <student@cs-520-3-4.local>
2021-11-01 10:29:50 -0400

    Added more content to test.cs

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
 	//SEAN MICK CHANGED SOME STUFF
+	//Elijah added some stuff
     // Start is called before the first frame update
     void Start()
     {
